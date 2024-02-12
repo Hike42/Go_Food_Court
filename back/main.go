f5b0cfb4642725567540cf04b8f59c650a0bfa4b
Baptiste VERDIER <baptiste.verdier@manomano.com>
2024-02-12 09:53:59 +0100

    feat(main): last commit & commented code

--- back/main.go
+++ back/main.go
@@ -56,27 +56,26 @@ type OrderStatus struct {
 }
 
 func initDB() *sql.DB {
-    username := os.Getenv("DB_USER")
-    password := os.Getenv("DB_PASSWORD")
-    host := os.Getenv("DB_HOST")
-    dbName := os.Getenv("DB_NAME")
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
 
-    dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?tls=true&interpolateParams=true", username, password, host, dbName)
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?tls=true&interpolateParams=true", username, password, host, dbName)
 
-    db, err := sql.Open("mysql", dataSourceName)
-    if err != nil {
-        log.Fatalf("Error opening database: %v", err)
-    }
-	
-    err = db.Ping()
-    if err != nil {
-        log.Fatalf("Error connecting to the database: %v", err)
-    }
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
 
-    fmt.Println("Connected to the database successfully.")
-    return db
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+
+	fmt.Println("Connected to the database successfully.")
+	return db
 }
-
 
 func graphqlHandler(schema graphql.Schema) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -109,7 +108,7 @@ func sendMail(to, subject, body string) error {
 	from := "baptiste.verd@gmail.com"
 	password := "cfsr ydwm ynby wmfq"
 
-	// Configurer le serveur SMTP de Gmail
+	// Config SMTP Gmail
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
 
@@ -133,28 +132,27 @@ func sendMail(to, subject, body string) error {
 	return nil
 }
 
-
 func main() {
-    if err := godotenv.Load(); err != nil {
-        log.Println("No .env file found")
-    }
-    db = initDB()
-    r := gin.Default()
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	db = initDB()
+	r := gin.Default()
 
-    config := cors.Config{
-        AllowOrigins: []string{
-            "https://master--gofoodcourt.netlify.app",
-            "http://localhost:3000",
-            "http://127.0.0.1:3000",
-        },
-        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-        AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
-        ExposeHeaders:    []string{"Content-Length"},
-        AllowCredentials: true,
-        AllowAllOrigins:  false,
-    }
+	config := cors.Config{
+		AllowOrigins: []string{
+			"https://master--gofoodcourt.netlify.app",
+			"http://localhost:3000",
+			"http://127.0.0.1:3000",
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowAllOrigins:  false,
+	}
 
-    r.Use(cors.New(config))
+	r.Use(cors.New(config))
 
 	schema, err := graphql.NewSchema(graphql.SchemaConfig{
 		Query:    RootQuery,
@@ -206,9 +204,9 @@ func main() {
 		}
 	})
 
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
-    r.Run(":" + port)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	r.Run(":" + port)
 }
